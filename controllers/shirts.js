ada99e41263abfb13f0f3635bcf2aa84f885e359
nicholasmrice <74073206+nicholasmrice@users.noreply.github.com>
2021-01-13 16:24:37 -0500

    Update shirts.js

--- controllers/shirts.js
+++ controllers/shirts.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose')
 
-const shirtSchema = new mongoose.Schema({
-  band: { type: String, required: true },
-  size: { type: String, required: true },
-  color: String,
-  image: String
-})
-const Shirt = mongoose.model('Shirt', shirtSchema)
-
-module.exports = Shirt
