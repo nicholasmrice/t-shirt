55a95e0581248d62052b5b8fd88275be1851c5e7
Nicholas Rice <nicholasmrice@gmail.com>
2021-01-13 15:49:12 -0500

    pushing server and env

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+app.use(express.json())
+app.use(express.static('public'))
+
+const animalsController = require('./controllers/shirts.js')
+app.use('/shirts', shirtsController)
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+mongoose.connection.on('error', (err) =>
+  console.log(err.message, ' is mongodb not running/Problem w Atlas connection?'))
+
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI))
+
+mongoose.connection.on('disconnected', () =>
+console.log('mongo disconnected'))
+
+app.listen(PORT, () => {
+  console.log(`listening on port, ${PORT}`);
+})
